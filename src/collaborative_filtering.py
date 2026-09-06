c90a6d38be8aa67646c9e8765b6618d836098077
Sovith <sovithkumarsingh786786@gmail.com>
2026-09-06 23:12:07 +0530

    dvc pipeline added

--- src/collaborative_filtering.py
+++ src/collaborative_filtering.py
@@ -8,7 +8,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 # output paths
 track_ids_save_path = "data/preprocessed/track_ids.npy"
 filtered_data_save_path = "data/preprocessed/collab_filtered_data.csv"
-interaction_matrix_save_path = "data/preprocessed/interaction_matrix.npz"
+sparse_matrix_save_path = "data/preprocessed/sparse_matrix.npz"
 # input paths
 songs_data_path = "data/processed/cleaned_data.csv"
 user_listening_history_data_path = "data/raw/User Listening History.csv"
@@ -86,11 +86,11 @@ def create_interaction_matrix(history_data:dd.DataFrame, track_ids_save_path, sa
     n_users = col_indices.nunique()
     
     # create the sparse matrix
-    interaction_matrix = csr_matrix((values, (row_indices, col_indices)), shape=(n_tracks, n_users))
+    sparse_matrix = csr_matrix((values, (row_indices, col_indices)), shape=(n_tracks, n_users))
     
     # save the sparse matrix
-    save_sparse_matrix(interaction_matrix, save_matrix_path)
-    
+    save_sparse_matrix(sparse_matrix, save_matrix_path)
+
     
 def collaborative_recommendation(song_name,artist_name,track_ids,songs_data,interaction_matrix,k=5):
     # lowercase the song name
@@ -152,7 +152,7 @@ def main():
     filter_songs_data(songs_data, unique_track_ids, filtered_data_save_path)
     
     # create the interaction matrix
-    create_interaction_matrix(user_data, track_ids_save_path, interaction_matrix_save_path)
+    create_interaction_matrix(user_data, track_ids_save_path, sparse_matrix_save_path)
 
 
 if __name__ == "__main__":
